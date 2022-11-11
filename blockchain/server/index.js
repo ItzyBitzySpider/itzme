fbe1922f9f40304f43ac12100c672e04b6d4ead8
Oceankoh <36119714+Oceankoh@users.noreply.github.com>
2022-11-11 12:34:49 +0800

    p2p websocket test

---
+++ blockchain/server/index.js
@@ -0,0 +1,8 @@
+import { httpServer, connect } from "./p2p.js"
+
+const port = 3000;
+httpServer.listen(port, () =>
+  console.log(`Example app listening on port ${port}!`)
+);
+
+connect("ws://143.198.209.169:3000")
--- blockchain/server/p2p.js
+++ blockchain/server/p2p.js
@@ -1,14 +1,89 @@
 //import socket
-import {Server} from 'socket.io';
-import * as http from 'http';
-import { app } from './api.js';
+import { Server } from "socket.io";
+import { io } from "socket.io-client";
+import * as http from "http";
+import { app } from "./api.js";
+import { Block, Chain } from "../chain/chain.js";
 const httpServer = http.createServer(app);
-const io = new Server(httpServer);
+const server = new Server(httpServer);
 
-io.on('connection', (socket)=>{
-    console.log('user connected');
-})
 
+let opened = [];
+let connected = [];
 
 const port = 3000;
-httpServer.listen(port, () => console.log(`Example app listening on port ${port}!`));
+const myAddress = "ws://localhost:3000";
+
+server.on("connection", (socket) => {
+  console.log("user connected");
+  // establishing mesh connection
+  socket.on("HANDSHAKE", (data) => {});
+  socket.on("NEW BLOCK", (data) => {
+    console.log("NEW BLOCK", data);
+  });
+  // socket.on("REQUEST CHECK", (data)=>{});
+  // socket.on("SEND CHECK", (data)=>{});
+  socket.on("REQUEST CHAIN", (data) => {});
+  socket.on("SEND CHAIN", (data) => {});
+});
+
+async function connect(address) {
+  if (
+    !connected.find((peer) => peer.address === address) &&
+    address !== myAddress
+  ) {
+    const socket = io(address);
+    socket.on("connect", () => {
+      socket.emit("HANDSHAKE", { peers: [myAddress, ...connected] });
+      opened.forEach((peer) =>
+        socket.emit("HANDSHAKE", { peers: [peer.address] })
+      );
+      if (
+        !opened.find((peer) => peer.address === address) &&
+        address !== myAddress
+      ) {
+        opened.push({ address, socket });
+      }
+      if (
+        !connected.find((peerAddress) => peerAddress === address) &&
+        address !== myAddress
+      ) {
+        connected.push(address);
+      }
+    });
+    //disconnected
+    socket.on("disconnect", () => {
+      opened = opened.filter((peer) => peer.address !== address);
+      connected = connected.filter((peer) => peer !== address);
+    });
+  }
+}
+
+async function broadcast(type, message) {
+  if (type === "NEW BLOCK") {
+    opened.forEach((peer) =>
+      peer.socket.emit("NEW BLOCK", {
+        data: message.data,
+        previousHash: message.previousHash,
+        hash: message.hash,
+        timestamp: message.timestamp,
+        type: message.type,
+        signature: message.signature,
+        issuerId: message.issuerId,
+      })
+    );
+  }
+  if (type === "REQUEST CHAIN") {
+    opened.forEach((peer) =>
+      peer.socket.emit("REQUEST CHAIN", {
+        data: message.address,
+      })
+    );
+  }
+}
+
+// httpServer.listen(port, () =>
+//   console.log(`Example app listening on port ${port}!`)
+// );
+
+export {broadcast, httpServer, connect};
