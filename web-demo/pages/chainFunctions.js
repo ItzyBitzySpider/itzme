cd32ace6f30167c5ad0c1242bc2c0ec9f7c53f0f
Ocean <36119714+Oceankoh@users.noreply.github.com>
2022-11-12 16:10:38 +0800

    blockchain interaction functions

--- web-demo/pages/chainFunctions.js
+++ web-demo/pages/chainFunctions.js
@@ -1,72 +1,201 @@
-import { useState, useEffect } from "react";
-import QRCode from "react-qr-code";
-
+import { useState, useEffect } from 'react';
 export default function ChainFunctions() {
-  const [url, setURL] = useState("http://143.198.209.169:3000");
+	const [url, setURL] = useState('http://143.198.209.169:3000');
+	const [value, setValue] = useState('');
+	const [option, setOption] = useState('menu');
+	const [blockNo, setBlockNo] = useState(0);
+	const [issuer, setIssuer] = useState('');
+	const [admin, setAdmin] = useState(false);
+	const [field, setField] = useState('');
+	const [identity, setIdentity] = useState('');
 
-  return (
-    <div className="h-screen w-screen flex flex-col justify-center items-center bg-slate-800">
-      <h1 className="text-6xl text-white"></h1>
-      <p className="text-2xl p-5 text-slate-400">
-        GUI to interact with blockchain.
-      </p>
-      <button
-        className="text-2xl font-medium bg-orange-600 p-3 px-10 rounded-lg  text-white"
-        onClick={() => {
-        }}
-      >
-        Generate
-      </button>
-      <p className=" text-white">{JSON.stringify(info)}</p>
-      <div className="h-2/3 flex flex-row w-screen">
-        <form className="h-full w-full flex flex-col items-center justify-center">
-          <div>
-            <div className="w-50 text-4xl  text-white flex items-center p-5">
-              <input
-                className="w-6 h-6 rounded-lg mr-5"
-                type="checkbox"
-                checked={name}
-                onChange={() => setName(!name)}
-                value="Name"
-                name="Name"
-              />
-              <p>Name</p>
-            </div>
-            <div className="w-50 text-4xl  text-white flex items-center p-5">
-              <input
-                className="w-6 h-6 rounded-lg mr-5"
-                type="checkbox"
-                checked={email}
-                onChange={() => setEmail(!email)}
-                value="Email"
-                name="Email"
-              />
-              Email
-            </div>
-            <div className="w-50 text-4xl  text-white flex items-center p-5">
-              <input
-                className="w-6 h-6 rounded-lg mr-5"
-                type="checkbox"
-                checked={nric}
-                onChange={() => setNric(!nric)}
-                value="NRIC"
-                name="NRIC"
-              />
-              NRIC
-            </div>
-          </div>
-        </form>
-        <div className="h-full w-full flex flex-col justify-center items-center">
-          <div className="bg-white p-4">
-            <QRCode
-              size={512}
-              // style={{ height: 'auto', maxWidth: '100%', width: '100%' }}
-              value={JSON.stringify(info)}
-              // viewBox={`0 0 256 256`}
-            />
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	const getBlockData = (blockNo) => {
+		fetch(url + '/getData?blockNo=' + blockNo)
+			.then((res) => res.json())
+			.then((data) => {
+				setValue(JSON.stringify(data));
+			});
+	};
+
+	const createIssuer = (issuer, admin) => {
+		fetch(
+			url + '/admin/createIssuer?name=' + issuer + '&admin=' + admin.toString()
+		)
+			.then((res) => res.json())
+			.then((data) => {
+				setValue(JSON.stringify(data));
+			});
+	};
+
+	const issueIdentity = (field, identity) => {
+		fetch(url + '/admin/issueIdentity?field' + field + '&value=' + identity)
+			.then((res) => res.json())
+			.then((data) => {
+				setValue(JSON.stringify(data));
+			});
+	};
+
+	return (
+		<div className='h-screen w-screen flex flex-col justify-center items-center bg-slate-800'>
+			<h1 className='text-6xl font-medium text-orange-500'>
+				Blockchain Scanner
+			</h1>
+			<p className='text-2xl p-5 text-slate-300'>
+				GUI to interact with blockchain.
+			</p>
+
+			<div className='h-2/3 flex flex-row w-screen mt-5'>
+				<div className='h-full w-full flex flex-col items-center justify-center'>
+					<div>
+						{option === 'menu' && (
+							<>
+								<div className='flex flex-col'>
+									<div className='p-5'>
+										<button
+											className='text-3xl font-medium p-3 px-10 rounded-lg hover:text-orange-400  text-white'
+											onClick={() => {
+												setOption('view');
+											}}>
+											View Blockchain Data &gt;
+										</button>
+									</div>
+									<div className='p-5'>
+										<button
+											className='text-3xl font-medium p-3 px-10 rounded-lg hover:text-orange-400 text-white'
+											onClick={() => {
+												setOption('create');
+											}}>
+											Create Issuer &gt;
+										</button>
+									</div>
+									<div className='p-5'>
+										<button
+											className='text-3xl font-medium  p-3 px-10 rounded-lg hover:text-orange-400  text-white'
+											onClick={() => {
+												setOption('issue');
+											}}>
+											Issue Identity &gt;
+										</button>
+									</div>
+								</div>
+							</>
+						)}
+						{option === 'view' && (
+							<div className='flex flex-col'>
+								<p className='text-2xl text-white font-medium py-2'>
+									Block Number
+								</p>
+
+								<input
+									className='w-50 rounded-lg p-3 text-xl'
+									onChange={(e) => setBlockNo(e.target.value)}
+									value={blockNo}
+								/>
+								<div className='flex justify-between'>
+									<button
+										className='text-3xl font-medium mt-10 rounded-lg hover:text-orange-400  text-white'
+										onClick={() => {
+											setOption('menu');
+										}}>
+										&lt; Back
+									</button>
+									<button
+										className='text-3xl font-medium mt-10 rounded-lg hover:text-orange-400  text-white'
+										onClick={() => {
+											getBlockData(blockNo);
+										}}>
+										Execute &gt;
+									</button>
+								</div>
+							</div>
+						)}
+						{option === 'create' && (
+							<div className='flex flex-col'>
+								<p className='text-2xl text-white font-medium py-2'>
+									Issuer Name
+								</p>
+								<input
+									className='w-50 rounded-lg p-3 text-xl'
+									onChange={(e) => setIssuer(e.target.value)}
+									value={issuer}
+								/>
+								<div className='flex items-center mt-6'>
+									<input
+										className='w-5 h-5 rounded-lg mr-5'
+										type='checkbox'
+										onChange={() => setAdmin(!admin)}
+										value={admin}
+									/>
+									<p className='text-2xl text-white font-medium py-2'>Admin</p>
+								</div>
+
+								<div className='flex justify-between'>
+									<button
+										className='text-3xl font-medium mt-10 rounded-lg hover:text-orange-400  text-white'
+										onClick={() => {
+											setOption('menu');
+										}}>
+										&lt; Back
+									</button>
+									<button
+										className='text-3xl font-medium mt-10 rounded-lg hover:text-orange-400  text-white'
+										onClick={() => {
+											createIssuer(issuer, admin);
+										}}>
+										Execute &gt;
+									</button>
+								</div>
+							</div>
+						)}
+						{option === 'issue' && (
+							<div className='flex flex-col'>
+								<p className='text-2xl text-white font-medium py-2'>
+									Field Name
+								</p>
+								<input
+									className='w-50 rounded-lg p-3 text-xl'
+									onChange={(e) => setField(e.target.value)}
+									value={field}
+								/>
+								<p className='mt-5 text-2xl text-white font-medium py-2'>Value</p>
+								<input
+									className='w-50 rounded-lg p-3 text-xl'
+									onChange={(e) => setIdentity(e.target.value)}
+									value={identity}
+								/>
+								<div className='flex justify-between'>
+									<button
+										className='text-3xl font-medium mt-10 rounded-lg hover:text-orange-400  text-white'
+										onClick={() => {
+											setOption('menu');
+										}}>
+										&lt; Back
+									</button>
+									<button
+										className='text-3xl font-medium mt-10 rounded-lg hover:text-orange-400  text-white'
+										onClick={() => {
+											issueIdentity(field, identity);
+										}}>
+										Execute &gt;
+									</button>
+								</div>
+							</div>
+						)}
+					</div>
+				</div>
+				<div className='h-full w-full flex flex-col justify-center items-center'>
+					<div className='h-full w-full flex flex-col items-center justify-center'>
+						<p className='text-3xl text-orange-400 font-semibold'>Result</p>
+						<textarea
+							rows={28}
+							cols={75}
+							className='rounded-2xl p-4 mt-5'
+							value={value}
+							readOnly
+						/>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
