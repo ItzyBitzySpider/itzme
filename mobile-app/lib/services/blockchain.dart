37652e86b1be40f053ea5c447fa65770ddca5620
MiloTruck <milotruckctf@gmail.com>
2022-11-13 05:39:11 +1300

    Modified getData to use /api/decrypt

--- mobile-app/lib/services/blockchain.dart
+++ mobile-app/lib/services/blockchain.dart
@@ -7,7 +7,7 @@ import 'package:itzme/models/credentials.dart';
 // IMPLEMENT SENDING OF PRIVATE KEY AND TXNO/BLOCKNO WITH SOCKETS
 
 socket.Socket? client;
-const String baseUrl = "http://143.198.209.169:3000";
+const String baseUrl = "143.198.209.169:3000";
 
 Future<Credentials> issueIdentity(Map<String, String> fields) async {
   var url = Uri.http(baseUrl, 'admin/issueIdentity', fields);
@@ -21,19 +21,35 @@ Future<Credentials> issueIdentity(Map<String, String> fields) async {
 }
 
 Future<String> getData(Credentials credentials) async {
-  var url = Uri.http('143.198.88.232:3000', 'api/decryptIdentity', {
-    'privateKey': credentials.privateKey,
-    'blockNo': credentials.blockNo.toString(),
-  });
+  var url = Uri.http(
+    baseUrl,
+    'getData',
+    {'blockNo': credentials.blockNo.toString()},
+  );
   var response = await http.get(url);
 
   if (response.statusCode == 200) {
-    return jsonDecode(jsonDecode(response.body))['value'];
+    final decoded = jsonDecode(response.body);
+    url = Uri.http('143.198.88.232:3000', 'api/decrypt');
+    response = await http.post(
+      url,
+      headers: {
+        'Content-type': 'application/json',
+        'Accept': 'application/json',
+      },
+      body: jsonEncode({
+        'encryptedData': decoded['data']['data'],
+        'privateKey': credentials.privateKey,
+      }),
+    );
+    if (response.statusCode == 200) {
+      return jsonDecode(jsonDecode(response.body))['value'];
+    } else {
+      throw Exception('Failed to decrypt data.');
+    }
+  } else {
+    throw Exception('Failed to retrieve data.');
   }
-
-  print(response.body);
-
-  return ' ';
 }
 
 void authorizeDetails(
