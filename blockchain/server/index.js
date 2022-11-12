83af556721508598f9da766011797e126aaa80cc
Ocean <36119714+Oceankoh@users.noreply.github.com>
2022-11-12 23:58:42 +0800

    typo

--- blockchain/server/index.js
+++ blockchain/server/index.js
@@ -3,8 +3,8 @@ import { httpServer, connect, opened, connected } from "./p2p.js";
 import * as fs from "fs";
 import * as yaml from "js-yaml";
 const config = yaml.load(fs.readFileSync("./server/config.yml", "utf8"));
-const peerAddress = config.peers;
-
+const peerAddress = config.peerAddress;
+console.log(peerAddress)
 if(peerAddress){
     connect(peerAddress, "REQUEST CHAIN");
 }
