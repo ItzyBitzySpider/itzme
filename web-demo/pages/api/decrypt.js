33ac7ded0b5567136e1f4b091bd08f4b698e04c0
Ocean <36119714+Oceankoh@users.noreply.github.com>
2022-11-12 18:11:09 +0800

    cors

--- web-demo/pages/api/decrypt.js
+++ web-demo/pages/api/decrypt.js
@@ -1,6 +1,13 @@
 import * as crypto from 'crypto';
+import NextCors from 'nextjs-cors';
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
+	await NextCors(req, res, {
+		// Options
+		methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+		origin: '*',
+		optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+	 });
 	const encryptedData = req.body.encryptedData;
 	const privateKey = crypto.createPrivateKey(req.body.privateKey);
 	const decryptedData = crypto.privateDecrypt(
