4457318a63c0c86b629effe9351e6ee440acff43
Ocean <36119714+Oceankoh@users.noreply.github.com>
2022-11-12 16:41:20 +0800

    decrypt function

--- web-demo/pages/chainFunctions.js
+++ web-demo/pages/chainFunctions.js
@@ -8,6 +8,7 @@ export default function ChainFunctions() {
 	const [admin, setAdmin] = useState(false);
 	const [field, setField] = useState('');
 	const [identity, setIdentity] = useState('');
+	const [idResult, setIdResult] = useState('');
 
 	const getBlockData = (blockNo) => {
 		fetch(url + '/getData?blockNo=' + blockNo)
@@ -35,6 +36,29 @@ export default function ChainFunctions() {
 			});
 	};
 
+	const decryptIdentity = async (jsonString) => {
+		const json = JSON.parse(jsonString);
+
+		const block = await fetch(url + '/getData?blockNo=' + json.blockNo);
+		const blockData = await block.json();
+		const encryptedData = blockData.data.data;
+
+		fetch('/api/decrypt', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				encryptedData: encryptedData,
+				privateKey: json.privateKey,
+			}),
+		})
+			.then((res) => res.json())
+			.then((data) => {
+				setValue(data);
+			});
+	};
+
 	return (
 		<div className='h-screen w-screen flex flex-col justify-center items-center bg-slate-800'>
 			<h1 className='text-6xl font-medium text-orange-500'>
@@ -46,7 +70,6 @@ export default function ChainFunctions() {
 
 			<div className='h-2/3 flex flex-row w-screen mt-5'>
 				<div className='h-full w-full flex flex-col items-center justify-center'>
-					<div>
 						{option === 'menu' && (
 							<>
 								<div className='flex flex-col'>
@@ -75,6 +98,15 @@ export default function ChainFunctions() {
 												setOption('issue');
 											}}>
 											Issue Identity &gt;
+										</button>
+									</div>
+									<div className='p-5'>
+										<button
+											className='text-3xl font-medium  p-3 px-10 rounded-lg hover:text-orange-400  text-white'
+											onClick={() => {
+												setOption('decrypt');
+											}}>
+											Decrypt Identity &gt;
 										</button>
 									</div>
 								</div>
@@ -157,7 +189,9 @@ export default function ChainFunctions() {
 									onChange={(e) => setField(e.target.value)}
 									value={field}
 								/>
-								<p className='mt-5 text-2xl text-white font-medium py-2'>Value</p>
+								<p className='mt-5 text-2xl text-white font-medium py-2'>
+									Value
+								</p>
 								<input
 									className='w-50 rounded-lg p-3 text-xl'
 									onChange={(e) => setIdentity(e.target.value)}
@@ -181,7 +215,36 @@ export default function ChainFunctions() {
 								</div>
 							</div>
 						)}
-					</div>
+						{option === 'decrypt' && (
+							<div className='flex flex-col w-2/3 h-2/3'>
+								<p className='text-2xl text-white font-medium py-2'>
+									Response from Blockchain
+								</p>
+								<textarea
+                  rows={28}
+                  cols={20}
+									className='w-50 rounded-lg p-3 text-xl'
+									onChange={(e) => setIdResult(e.target.value)}
+									value={idResult}
+								/>
+								<div className='flex justify-between'>
+									<button
+										className='text-3xl font-medium mt-10 rounded-lg hover:text-orange-400  text-white'
+										onClick={() => {
+											setOption('menu');
+										}}>
+										&lt; Back
+									</button>
+									<button
+										className='text-3xl font-medium mt-10 rounded-lg hover:text-orange-400  text-white'
+										onClick={() => {
+											decryptIdentity(idResult);
+										}}>
+										Execute &gt;
+									</button>
+								</div>
+							</div>
+						)}
 				</div>
 				<div className='h-full w-full flex flex-col justify-center items-center'>
 					<div className='h-full w-full flex flex-col items-center justify-center'>
